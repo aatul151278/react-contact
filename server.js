bce20afa70b77c8cba64a7b25704a53a73dd28cf
Atul <aatul151.prajapati@gmail.com>
2022-10-29 11:50:01 +0530

    update

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const dotenv = require("dotenv");
-dotenv.config();
 const path = require("path");
 
 const app = express();
-app.use(express.static(__dirname + "/build"));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/build/index.html"));
-});
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
 
 var port = process.env.PORT || 8005;
 
