35b4ce4dd713fa1264f68e4e1d296fd36d14decf
Atul <aatul151.prajapati@gmail.com>
2022-10-29 11:20:49 +0530

    update package.json

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const dotenv =require('dotenv');
-dotenv.config()
-const path = require('path');
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const path = require("path");
 
 const app = express();
-app.use(express.static(__dirname + '/build'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
+app.use(express.static(__dirname + "/build"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
-var port = process.env.PORT || 8000;
 
-app.listen(port,function(){
-  console.log("Front started",port)
+var port = process.env.PORT || 5005;
+
+app.listen(port, function () {
+  console.log("Front started", port);
 });
